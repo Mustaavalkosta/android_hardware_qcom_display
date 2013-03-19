70007e2d081ed60e356c8c5b5c64dc4e650fdda9
Naseer Ahmed <naseer@codeaurora.org>
2013-03-18 20:14:05 -0400

    hwc: Add support for plane alpha
    HWC 1.2 API passes down the plane alpha and the blending
    operation to apply. Pass this information to the MDP.
    
    Change-Id: Ie191b7414b7100a951a33c02291d8f5b7dab7645
    
    Conflicts:
    	libhwcomposer/hwc_utils.cpp

--- libhwcomposer/hwc_fbupdate.cpp
+++ libhwcomposer/hwc_fbupdate.cpp
@@ -105,11 +105,15 @@ bool FBUpdateLowRes::configure(hwc_context_t *ctx,
         ovutils::eIsFg is_fg =
            ctx->mVidOv[mDpy]->isModeOn()? ovutils::IS_FG_OFF:ovutils::IS_FG_SET;
 
+        //XXX: FB layer plane alpha is currently sent as zero from
+        //surfaceflinger
         ovutils::PipeArgs parg(mdpFlags,
                 info,
                 z_order,
                 is_fg,
-                ovutils::ROT_FLAGS_NONE);
+                ovutils::ROT_FLAGS_NONE,
+                ovutils::DEFAULT_PLANE_ALPHA,
+                (ovutils::eBlending) getBlending(layer->blending));
         ov.setSource(parg, dest);
 
         hwc_rect_t sourceCrop;
@@ -222,11 +226,15 @@ bool FBUpdateHighRes::configure(hwc_context_t *ctx,
         ovutils::eIsFg is_fg =
            ctx->mVidOv[mDpy]->isModeOn()? ovutils::IS_FG_OFF:ovutils::IS_FG_SET;
 
+        //XXX: FB layer plane alpha is currently sent as zero from
+        //surfaceflinger
         ovutils::PipeArgs pargL(mdpFlagsL,
                 info,
                 z_order,
                 is_fg,
-                ovutils::ROT_FLAGS_NONE);
+                ovutils::ROT_FLAGS_NONE,
+                ovutils::DEFAULT_PLANE_ALPHA,
+                (ovutils::eBlending) getBlending(layer->blending));
         ov.setSource(pargL, destL);
 
         ovutils::eMdpFlags mdpFlagsR = mdpFlagsL;
@@ -235,7 +243,9 @@ bool FBUpdateHighRes::configure(hwc_context_t *ctx,
                 info,
                 z_order,
                 is_fg,
-                ovutils::ROT_FLAGS_NONE);
+                ovutils::ROT_FLAGS_NONE,
+                ovutils::DEFAULT_PLANE_ALPHA,
+                (ovutils::eBlending) getBlending(layer->blending));
         ov.setSource(pargR, destR);
 
         hwc_rect_t sourceCrop;
