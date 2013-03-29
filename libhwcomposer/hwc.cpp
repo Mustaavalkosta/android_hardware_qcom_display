dd417cc88388bc6c778a67cb76f6cd6e0035981d
Arun Kumar K.R <akumarkr@codeaurora.org>
2013-03-28 18:41:06 -0700

    hwc: clean up overlay on external display blank
    - On suspend, SurfaceFlinger calls hwc_blank on primary and
      external separately, inbetween these calls there might be a
      update, which leads to acquiring overlay pipes for external
      which will not be cleared and results in showing up stale
      frame on external display
    - This change clears all the overlay pipes used on every blank
    
    Change-Id: I44d62b7d0fcd992cf8a26bb0c21df77952bc1381
    CRs-fixed: 464070
    
    Conflicts:
    
    	libhwcomposer/hwc.cpp
    
    Conflicts:
    	libhwcomposer/hwc.cpp

--- libhwcomposer/hwc.cpp
+++ libhwcomposer/hwc.cpp
@@ -252,31 +252,21 @@ static int hwc_blank(struct hwc_composer_device_1* dev, int dpy, int blank)
     int ret = 0;
     ALOGD("%s: %s display: %d", __FUNCTION__,
           blank==1 ? "Blanking":"Unblanking", dpy);
+    if(blank) {
+        // free up all the overlay pipes in use
+        // when we get a blank for either display
+        // makes sure that all pipes are freed
+        ctx->mOverlay->configBegin();
+        ctx->mOverlay->configDone();
+        ctx->mRotMgr->clear();
+    }
     switch(dpy) {
         case HWC_DISPLAY_PRIMARY:
             if(blank) {
-                ctx->mOverlay->configBegin();
-                ctx->mOverlay->configDone();
-                ctx->mRotMgr->clear();
-                ret = ioctl(m->framebuffer->fd, FBIOBLANK, FB_BLANK_POWERDOWN);
-
-                if(ctx->dpyAttr[HWC_DISPLAY_VIRTUAL].connected == true) {
-                    // Surfaceflinger does not send Blank/unblank event to hwc
-                    // for virtual display, handle it explicitly when blank for
-                    // primary is invoked, so that any pipes unset get committed
-                    if (!ctx->mExtDisplay->post()) {
-                        ret = -1;
-                        ALOGE("%s:post failed for virtual display !!",
-                                                            __FUNCTION__);
-                    } else {
-                        ctx->dpyAttr[HWC_DISPLAY_VIRTUAL].isActive = !blank;
-                    }
-                }
+                ret = ioctl(ctx->dpyAttr[dpy].fd, FBIOBLANK,
+                            FB_BLANK_POWERDOWN);
             } else {
                 ret = ioctl(m->framebuffer->fd, FBIOBLANK, FB_BLANK_UNBLANK);
-                if(ctx->dpyAttr[HWC_DISPLAY_VIRTUAL].connected == true) {
-                    ctx->dpyAttr[HWC_DISPLAY_VIRTUAL].isActive = !blank;
-                }
             }
             break;
         case HWC_DISPLAY_EXTERNAL:
